789dd68f66eea8b1dd5b612bd6d18591306af5f4
Keenan Graham <keenangraham@stanford.edu>
2020-05-28 11:03:43 -0700

    Add debug log

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,11 @@ use connection::Connection;
 #[macro_use]
 extern crate lazy_static;
 
+#[macro_use]
+extern crate log;
 
 fn main() {
+    env_logger::init();
     let token = env::var("RUSTY_BOT_TOKEN").unwrap();
     Connection::new(&token).listen();
 }
